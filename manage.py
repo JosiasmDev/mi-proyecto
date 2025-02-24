d222c00b55639e2b7f660efbad298c9d99ac48a5
JosiasmDev <josiasdicampus@gmail.com>
2025-02-24 10:03:46 +0100

    a

--- manage.py
+++ manage.py
@@ -15,7 +15,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    
+    # Obtén el puerto desde la variable de entorno, si está presente
+    port = os.getenv('PORT', 8000)  # Si no existe, usará 8000 como valor por defecto
+
+    # Ejecuta el servidor en el puerto correcto
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
 
 
 if __name__ == '__main__':
